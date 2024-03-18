85ee5b26b37d006882d69c1133ee204b0ec30e4e
arathysn <arathy.nair@youlend.com>
2024-03-18 11:40:12 +0000

    fix: 26798: corrected the init file

--- agent_qa.tf
+++ agent_qa.tf
@@ -78,7 +78,7 @@ resource "aws_launch_template" "agent_qa_lt" {
   ebs_optimized = false
 
   instance_type = var.qa_agent_instance_type
-  user_data     = data.template_cloudinit_config.agent_qa_init.rendered
+  user_data     = data.template_cloudinit_config.qa_agent_init.rendered
 
   monitoring {
     enabled = true
